a1cb3f29a0386804a87b85d48d41747333a9f59f
r0ute <route-a@ya.ru>
2026-09-03 22:36:17 -0500

    fix: update index page to include beta status badge and correct GitHub link formatting

--- generator/renderer.py
+++ generator/renderer.py
@@ -84,9 +84,8 @@ def write_index_page(
         "and other game data.",
         "",
         f"![Game Version](https://img.shields.io/badge/Game%20Version-{version}-black?logo=unrealengine)",
-        "[![GitHub](https://img.shields.io/badge/Source%20Code-GitHub-black?logo=github)]"
-        "(https://github.com/r0ute/bw-wiki)",
-        "",
+        "[![GitHub](https://img.shields.io/badge/Source%20Code-GitHub-black?logo=github)](https://github.com/r0ute/bw-wiki)",
+        "![Beta](https://img.shields.io/badge/Status-Beta-orange)",
         *_render_data(page_groups),
     ]
 
